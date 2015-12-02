024b5f367d01908b31ed6b6b1f50e82df57ed0f2
stephds01 <stephds01@gmail.com>
2015-12-02 11:05:46 +0100

    Update exo10 && exo11

---
+++ Exo_chagnon/js/exo11.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Stéphanie on 02/12/2015.
+ */
+function Transform() {
+
+var text = document.getElementById('text');
+    text.style.fontSize='Verdana';
+    text.style.textDecoration='line-through';
+    text.style.fontSize='150%';
+    text.style.lineHeight='2';
+    text.style.textTransform='capitalize';
+}
