9159053a823bd8d87104f164e5896d617672d530
stephds01 <stephds01@gmail.com>
2015-12-03 21:58:09 +0100

    Update exo1

--- exo1 -Simplon/js/exo1.js
+++ exo1 -Simplon/js/exo1.js
@@ -1,3 +1,50 @@
 /**
  * Created by Stéphanie on 03/12/2015.
  */
+
+    /*      --  EXEMPLES  --    */
+// On crée un objet
+var contact = {
+    nom : 'DESA',
+    prenom : 'Stéphanie',
+    tel : '06 75 74 32 01'
+
+};
+alert(contact.nom);
+alert(contact.prenom);
+alert(contact.tel);
+
+
+//On crée un tableau objet
+
+var contacts = [
+    {
+    nom: 'GOUJON',
+    prenom: 'MArie',
+    },
+    {
+    nom: 'DSA',
+        prenom: 'Steph'
+    }
+];
+
+alert(contacts[0].nom);
+alert(contacts[1].prenom);
+
+
+/*      ---Exercices---     */
+//Je crée un tableau Contact
+var tabContact = [
+    {
+        prenom: 'Stéphanie',
+        nom: 'DESA',
+        numero: '06 75 74 32 01'
+    },
+    {
+        prenom: 'Alfredo',
+        nom: 'DESA',
+        numero: '06 80 46 38 42'
+    }
+];
+
+
